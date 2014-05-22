dd7e24605c2517863147605ae638696581d030a0
njsekay <njsekay@gmail.com>
2014-05-22 16:18:03 +0900

    modify mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'syssqu.herokuapp.com' }
+  config.action_mailer.default_url_options = {:host => 'syssqu.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,   # I've also tried :login
+    :enable_starttls_auto => true,  # Also tried tls => true
+    :user_name => 'syssqu@gmail.com',
+    :password => 'Squareadm11#'
+  }
 end
--- config/initializers/gmail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'herokuapp.com',
-  :user_name            => 'syssqu@gmail.com', # full email address (user@your.host.name.com)
-  :password             => 'Squareadm11#',
-  :authentication       => :plain,
-  :enable_starttls_auto => true
-}
