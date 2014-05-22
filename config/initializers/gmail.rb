1c1e9d7155d0fece69db9bfb9762ae0fe20e73db
njsekay <njsekay@gmail.com>
2014-05-22 16:09:31 +0900

    modify mail setting

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => 'syssqu.herokuapp.com',
+  :domain               => 'herokuapp.com',
   :user_name            => 'syssqu@gmail.com', # full email address (user@your.host.name.com)
   :password             => 'Squareadm11#',
-  :authentication       => 'login',
+  :authentication       => :plain,
   :enable_starttls_auto => true
 }
