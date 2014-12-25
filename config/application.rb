0eb4a32fb701a0a7cbc5197945f0fe71bb5e4793
njsekay <njsekay@gmail.com>
2014-12-25 14:59:00 +0900

    temporary commit

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Syssqu
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
-    config.assets.initialize_on_precompile = true
+    #config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
